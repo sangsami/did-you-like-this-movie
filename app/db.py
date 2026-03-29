7d4a50b63a55e4e14f7e1d0010463e2fda1db76e
Sangsami <dinh.sami@gmail.com>
2026-03-29 23:03:35 +0300

    feat: add app and database initialization

---
+++ app/db.py
@@ -0,0 +1,66 @@
+import sqlite3
+from datetime import datetime
+
+import click
+from flask import current_app, g
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+@click.command('seed-db')
+def seed_db_command():
+    db = get_db()
+
+    db.execute("DELETE FROM movies")
+
+    with current_app.open_resource('data/movies.txt') as f:
+        movies = [(line.decode('utf-8').strip(),) for line in f]
+
+    db.executemany(
+        "INSERT OR IGNORE INTO movies (title) VALUES (?)",
+        movies
+    )
+
+    db.commit()
+
+    click.echo('Seeded the database from file.')
+
+
+sqlite3.register_converter(
+    "timestamp", lambda v: datetime.fromisoformat(v.decode())
+)
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+    app.cli.add_command(seed_db_command)
